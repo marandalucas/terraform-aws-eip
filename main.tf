3eb0df25cb0b17dee5e83d43bd4f772dfbb1971b
Marco Aranda <marco.aranda@orangebank.es>
2022-06-16 12:14:10 +0200

    Format files

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_eip" "eip" {
-    vpc = var.vpc
+  vpc = var.vpc
 
-    instance = var.instance
-    network_interface = var.network_interface
-    associate_with_private_ip = var.associate_with_private_ip
-    tags = var.tags
+  instance                  = var.instance
+  network_interface         = var.network_interface
+  associate_with_private_ip = var.associate_with_private_ip
+  tags                      = var.tags
 
-    public_ipv4_pool = var.public_ipv4_pool
+  public_ipv4_pool = var.public_ipv4_pool
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "vpc" {
-    description = "Whether the EIP is in a VPC."
-    type = bool
-    default = true
+  description = "Whether the EIP is in a VPC."
+  type        = bool
+  default     = true
 }
 
 variable "instance" {
-    description = "Instance to associate with EIP."
-    type = string
-    default = null
+  description = "Instance to associate with EIP."
+  type        = string
+  default     = null
 }
 
 variable "network_interface" {
-    description = "ENI to associate with EIP."
-    type = string
-    default = null
+  description = "ENI to associate with EIP."
+  type        = string
+  default     = null
 }
 
 variable "associate_with_private_ip" {
-    description = "Private IP to associate with EIP."
-    type = string
-    default = null
+  description = "Private IP to associate with EIP."
+  type        = string
+  default     = null
 }
 variable "tags" {
-    description = "Tags to associate with ENI."
-    type = map(string)
-    default = {}
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
 }
 
 variable "public_ipv4_pool" {
-    description = "IP pool to use (or 'amazon'.)"
-    type = string
-    default = "amazon"
+  description = "IP pool to use (or 'amazon'.)"
+  type        = string
+  default     = "amazon"
 }
 
